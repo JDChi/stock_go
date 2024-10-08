4d66330964c1be1d0ef1a6013005e84a60ed358c
jdchi <chijiaduo@hotmail.com>
2024-10-08 08:49:34 +0800

    test: update test

--- core/formula/formula_test.go
+++ core/formula/formula_test.go
@@ -3,12 +3,15 @@ package formula
 import "testing"
 
 func TestCalPERatioOfTheStockMarket(t *testing.T) {
-	result := CalPERatioOfTheStockMarket(0.04, 0.06)
-	t.Logf("%+v", result)
-
+	result := CalPERatioOfTheStockMarket(0.0135, 0.0335)
+	t.Logf("min: %f", result.MinPERatio)
+	t.Logf("max: %f", result.MaxPERatio)
+	t.Logf("reasonable: %f", result.ReasonablePERatio)
+	t.Logf("current SH: %f", 14.24)
+	t.Logf("current SZ: %f", 23.68)
 }
 
 func TestCalTheValuationOfTheStock(t *testing.T) {
-	result := CalTheValuationOfTheStock(0.33, 0.0135)
-	t.Log(result)
+	result := CalTheValuationOfTheStock(1.3, 0.0135)
+	t.Logf("value: %f", result)
 }
